c13daa5849ad5f105504e7d79948a73bbba15ba3
Rin <rin.t.4921@gmail.com>
2021-09-17 17:55:17 +0900

    first commit

---
+++ WebViewSample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WebViewSample
+//
+//  Created by Rin on 2021/09/17.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions:
+                        [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let navigationController =
+            UINavigationController(rootViewController: ViewController()) // ←ここ変えるとMVPとか切り替えられる
+        navigationController.isNavigationBarHidden = true
+        self.window?.rootViewController = navigationController
+        self.window?.makeKeyAndVisible()
+        return true
+    }
+}
+
