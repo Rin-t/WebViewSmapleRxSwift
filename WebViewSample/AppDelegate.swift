b49a9c13810315714c4bd49da5462400027a4a9f
Rin <rin.t.4921@gmail.com>
2021-09-17 19:13:47 +0900

    KVOで実装

--- WebViewSample/AppDelegate.swift
+++ WebViewSample/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.window = UIWindow(frame: UIScreen.main.bounds)
         let navigationController =
             UINavigationController(rootViewController: ViewController()) // ←ここ変えるとMVPとか切り替えられる
-        navigationController.isNavigationBarHidden = true
+        // navigationController.isNavigationBarHidden = true
         self.window?.rootViewController = navigationController
         self.window?.makeKeyAndVisible()
         return true
--- WebViewSample/ViewController.swift
+++ WebViewSample/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var progressView: UIProgressView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupWebView()
     }
 
+    private func setupWebView() {
+        // webView.isLoadingの値の変化を監視
+        webView.addObserver(self, forKeyPath: "loading", options: .new, context: nil)
 
+        // webView.estimatedProgressの値の変化を監視
+        webView.addObserver(self, forKeyPath: "estimatedProgress", options: .new, context: nil)
+
+        let url = URL(string: "https://www.google.com/?hl=ja")
+        let urlRequest = URLRequest(url: url!)
+        webView.load(urlRequest)
+        progressView.setProgress(0.1, animated: true)
+    }
+
+    deinit {
+        webView.removeObserver(self, forKeyPath: "loading")
+        webView.removeObserver(self, forKeyPath: "estimatedProgress")
+    }
+
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "loading" {
+            UIApplication.shared
+                .isNetworkActivityIndicatorVisible = webView.isLoading
+            if !webView.isLoading {
+                progressView.setProgress(0.0, animated: false)
+                navigationItem.title = webView.title
+            }
+        }
+
+        if keyPath == "estimatedProgress" {
+            progressView
+                .setProgress(Float(webView.estimatedProgress), animated: true)
+        }
+    }
 }
 
